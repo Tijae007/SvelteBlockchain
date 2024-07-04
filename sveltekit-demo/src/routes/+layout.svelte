4b101611bc296525e068ee1cba61a72613374263
Yunus <tijaniyunus07@gmail.com>
2024-07-04 19:12:43 +0200

    Added changes

---
+++ sveltekit-demo/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+
+import Navbar from "$lib/Navbar.svelte";
+import Footer from "$lib/Footer.svelte";
+import "../app.css"
+
+</script>
+
+<Navbar />
+<slot />
+<Footer />
+
--- sveltekit-demo/src/routes/+page.svelte
+++ sveltekit-demo/src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class = "font-extrabold underline bg-red-700 text-white pt-5 ml-1">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
